2e9a846a095fc38f0c99f04a5e1580e5736b0b27
Eitan-brightleaf <eitan@brightleafc.com>
2024-12-24 15:33:02 +0200

    working on twilio phone

---
+++ class-twilio-phone-for-wp.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * The file that defines the core plugin class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       https://digital.brightleaf.info
+ * @since      1.0.0
+ *
+ * @package    Twilio_phone_for_wp
+ * @subpackage Twilio_phone_for_wp/includes
+ */
+
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    Twilio_phone_for_wp
+ * @subpackage Twilio_phone_for_wp/includes
+ * @author     Brightleaf Digital <eitan@brightleafc.com>
+ */
+class Twilio_phone_for_wp {
+
+	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 */
+	protected $plugin_name;
+
+	/**
+	 * The current version of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+	protected $version;
+
+	/**
+	 * Define the core functionality of the plugin.
+	 *
+	 * Set the plugin name and the plugin version that can be used throughout the plugin.
+	 * Load the dependencies, define the locale, and set the hooks for the admin area and
+	 * the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct() {
+		if ( defined( 'TWILIO_PHONE_FOR_WP_VERSION' ) ) {
+			$this->version = TWILIO_PHONE_FOR_WP_VERSION;
+		} else {
+			$this->version = '1.0.0';
+		}
+		$this->plugin_name = 'twilio_phone_for_wp';
+	}
+
+
+	/**
+	 * Run the loader to execute all of the hooks with WordPress.
+	 *
+	 * @since    1.0.0
+	 */
+	public function run() {
+
+	}
+
+	/**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The name of the plugin.
+	 */
+	public function get_plugin_name() {
+		return $this->plugin_name;
+	}
+
+
+	/**
+	 * Retrieve the version number of the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The version number of the plugin.
+	 */
+	public function get_version() {
+		return $this->version;
+	}
+}
--- twilio_phone_for_wp.php
+++ twilio_phone_for_wp.php
@@ -21,48 +21,20 @@
  * Author URI:        https://digital.brightleaf.info/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       twilio_phone_for_wp
- * Domain Path:       /languages
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
 define( 'TWILIO_PHONE_FOR_WP_VERSION', '1.0.0' );
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-twilio_phone_for_wp-activator.php
- */
-function activate_twilio_phone_for_wp() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-twilio_phone_for_wp-activator.php';
-	Twilio_phone_for_wp_Activator::activate();
-}
 
 /**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-twilio_phone_for_wp-deactivator.php
+ * The core plugin class.
  */
-function deactivate_twilio_phone_for_wp() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-twilio_phone_for_wp-deactivator.php';
-	Twilio_phone_for_wp_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_twilio_phone_for_wp' );
-register_deactivation_hook( __FILE__, 'deactivate_twilio_phone_for_wp' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-twilio_phone_for_wp.php';
+require plugin_dir_path( __FILE__ ) . '/class-twilio-phone-for-wp.php';
 
 /**
  * Begins execution of the plugin.
@@ -73,7 +45,8 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-twilio_phone_for_wp.php';
  *
  * @since    1.0.0
  */
-function run_twilio_phone_for_wp() {
+function run_twilio_phone_for_wp(): void
+{
 
 	$plugin = new Twilio_phone_for_wp();
 	$plugin->run();
